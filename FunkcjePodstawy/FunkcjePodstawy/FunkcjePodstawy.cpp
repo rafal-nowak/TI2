f5d856d2d4f6710a74a4a1a10267556862fb0c62
Rafal Nowak <contact@rafal-nowak.com>
2020-03-22 22:23:43 +0100

    Dodano projekt FunkcjePodstawy

---
+++ FunkcjePodstawy/FunkcjePodstawy/FunkcjePodstawy.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+// typ_wartosci_zwracanej nazwa_funkcji(lista_parametrow)
+
+int main(void)
+{
+	int liczba_1 = 2, liczba_2 = 5, wynik = 0;
+
+	printf_s("Program ten sluzy do obliczania sumy dwoch liczb\n");
+	printf_s("Wpraowdz liczby ktore chcesz zsumowac (rozdziel je spacja)\n");
+	scanf_s("%d %d", &liczba_1, &liczba_2);
+	wynik = liczba_1 + liczba_2;
+	printf_s("Suma wprowadzonych liczb wynosi %d\n", wynik);
+
+	return 0;
+}
